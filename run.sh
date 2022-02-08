d54fdf1127d8c625019303ca2344faa4814e5d1d
Hansu Kim <cpm0722@gmail.com>
2022-02-09 00:24:22 +0900

    Update run.sh: change python file name

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ BASE_URL="https://huqeyhi95c.execute-api.ap-northeast-2.amazonaws.com/prod"
 MATCH_SKILL=10000
 WAIT_WEIGHT=2
 
-python3 main.py --problem 1 --init-token $INIT_TOKEN --base-url $BASE_URL --match-skill 20000 --wait-weight 3
-python3 main.py --problem 2 --init-token $INIT_TOKEN --base-url $BASE_URL --match-skill 10000 --wait-weight 4
+python3 solve.py --problem 1 --init-token $INIT_TOKEN --base-url $BASE_URL --match-skill 20000 --wait-weight 3
+python3 solve.py --problem 2 --init-token $INIT_TOKEN --base-url $BASE_URL --match-skill 10000 --wait-weight 4
